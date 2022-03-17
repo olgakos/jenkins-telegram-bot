c4801c3d3ff5723c8b5eed410107410308c98094
Olga <ok2304@yandex.ru>
2022-03-17 17:50:29 +0300

    Add 4 Simple Tests (+Allure)

---
+++ src/test/java/com/github/olgakos/tests/simple/SimpleYesNotSkippedTests.java
@@ -0,0 +1,32 @@
+package com.github.olgakos.tests.simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@DisplayName("It's 3 Simple Yes-Not-Skipped Tests")
+public class SimpleYesNotSkippedTests {
+
+    @Test
+    @Tag("Positive Test One (Tag)")
+    @DisplayName("Positive Test One")
+    void test00() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("False Test: With some reason")
+    void test01() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled("Skipped Test: With some reason")
+    void test02() {
+        assertTrue(true);
+    }
+}
